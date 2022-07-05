3034bac2f3cc2f7770d493c37e7d4183fe6c42fe
BillBrousalis <bill.brousalis@gmail.com>
2022-07-05 17:17:28 +0300

    trash

--- source/irq_handler.c
+++ source/irq_handler.c
@@ -41,7 +41,13 @@ void ADC1_IRQHANDLER(void) {
   	}
   }
   /* Place your code here */
-  pot_update(result_values[0], result_values[1]);
+  /* toggle pot channel (0 - 1) */
+  chnl ^= 1;
+  if(chnl == 0)
+    ADC16_SetChannelConfig(ADC1_PERIPHERAL, ADC1_CH0_CONTROL_GROUP, &ADC1_channelsConfig[0]);
+  else
+    ADC16_SetChannelConfig(ADC1_PERIPHERAL, ADC1_CH1_CONTROL_GROUP, &ADC1_channelsConfig[1]);
+  pot_update(chnl, result_values[0]);
 
   /* Add for ARM errata 838869, affects Cortex-M4, Cortex-M4F
      Store immediate overlapping exception return operation might vector to incorrect interrupt. */
